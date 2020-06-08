8cd872e49e1659695059c6c41f69d47db89f01ea
Sagar Jadhav <sagarjadhavlive@gmail.com>
2020-06-08 11:44:20 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -15,5 +15,11 @@ self.addEventListener('install', function(e) {
 });
 
 self.addEventListener('fetch', function(event) {
- alert(event.request.url);
+ console.log(event.request.url);
+
+ event.respondWith(
+   caches.match(event.request).then(function(response) {
+     return response || fetch(event.request);
+   })
+ );
 });
