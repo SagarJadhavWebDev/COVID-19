00d99b1675fff88ed1805d3f9be19591d13246cd
Sagar Jadhav <sagarjadhavlive@gmail.com>
2020-06-08 11:41:46 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+importScripts('/cache-polyfill.js');
+
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '/',
+       'index.html',
+       'COVID-19-TRACKER-BY-COUNTRY.htm',
+       'Covid.css',
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(event) {
+ alert(event.request.url);
+});
